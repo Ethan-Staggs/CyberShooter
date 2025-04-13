0e57c71658491e04355cb83a659dddaaa12f9f71
Ethan Staggs <78055596+Ethan-Staggs@users.noreply.github.com>
2025-04-13 19:56:36 -0400

    General setup

---
+++ cyber-shooter/bullet.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@export var speed: float = 500
+var bulletDirection = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func initialize(isPlayerFlipped: bool) -> void:
+	if isPlayerFlipped == true:
+		speed = speed
+	else:
+		speed = -speed
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position += Vector2(speed * delta, 0)
--- cyber-shooter/player.gd
+++ cyber-shooter/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
-
+#@export var Bullet: PackedScene
 const SPEED = 100.0
-const JUMP_VELOCITY = -215.0
+const JUMP_VELOCITY = -345.0
 var gravity = 800
+var isPlayerFlipped = true
 
 
 func _physics_process(delta: float) -> void:
@@ -17,7 +18,8 @@ func _physics_process(delta: float) -> void:
 	
 	#Handle shoot
 	if Input.is_action_just_pressed("shoot"):
-		print("Shoot")
+		shoot(isPlayerFlipped)
+		print(isPlayerFlipped)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -26,14 +28,22 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("left"):
 		direction.x -= 1
 		$AnimatedSprite2D.flip_h = false
+		isPlayerFlipped = false
 		$AnimatedSprite2D.play("run")
 	elif  Input.is_action_pressed("right"):
 		direction.x += 1
 		$AnimatedSprite2D.flip_h = true
+		isPlayerFlipped = true
 		$AnimatedSprite2D.play("run")
 	else:
 		$AnimatedSprite2D.play("idle")
 		
 	velocity.x = direction.x * SPEED
 	move_and_slide()
+	
+func shoot(isPlayerFlipped):
+	var bullet = preload("res://bullet.tscn").instantiate()
+	bullet.initialize(isPlayerFlipped)
+	bullet.global_position = $AnimatedSprite2D/Marker2D.global_position
+	get_tree().root.add_child(bullet)
 	
